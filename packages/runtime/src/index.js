62a3c12ebd153a8e29e6e1a39652e9ddf2be5571
Erasmus <refugedesigns17@gmail.com>
2023-09-13 14:56:04 +0000

    completed mountDom and destroyDom functionality -> ch04

--- packages/runtime/src/index.js
+++ packages/runtime/src/index.js
@@ -9,14 +9,3 @@ hFragment([
   ]),
 ]);
 
-function lipsum(num) {
-  const lorem = `Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-                tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
-                veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
-                commodo consequat`;
-  const allTexts = [];
-  for (let i = 0; i < num; i++) {
-    allTexts(h("p", {}, lorem));
-  }
-  return hFragment(allTexts);
-}
